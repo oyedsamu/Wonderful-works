2a9f50102fb77f46e36e51c314944fdbde277dbe
oyedsamu <54637620+oyedsamu@users.noreply.github.com>
2019-09-23 22:18:42 +0100

    Add files via upload

---
+++ Decagon first day.js
@@ -0,0 +1,23 @@
+// let myFullName = "Samuel Oyedele";
+// let myFirstObject = {
+//     Name : "Oyedele",
+//     PhoneNumber: 08162339425,
+//     Location : "Lagos",
+//     Age : 28,
+//     Membership : true, 
+//     Colours: ['blue', 'red', 'white', 'pink', 'yellow']
+// }
+
+// myFirstObject.Colours[5] = "blue";
+// console.log(myFirstObject.Colours);
+
+var myFruits = [ "banana", "carrot", "guava", "apple"]
+console.log(myFruits[myFruits.length-1]);
+console.log(myFruits.join()); 
+
+
+
+
+
+
+
---
+++ functions.js
@@ -0,0 +1,42 @@
+// function greet() {
+//     console.log("hello pal")
+// };
+
+// greet()
+
+// const speak = function (){          //This is an expression having = sign. It must end with a ;
+
+// }
+
+// /* A function declaration is hoisted directly to the top of the code while
+// a variable expression of a function is not hoisted. This means you can effectively
+// declare your function at the very end but still call it prior to its place in the 
+// code*/
+
+// /*The argument/ parameter in the () immediately after a function decalrartion is like a locally
+// decalred variable which is expected to work within the function alone. It is 
+// expected to hold a value when you're using it in a calling of a function, 
+// else it returns undefined */
+
+// /* When you need more than one value to be used in a function, you declare them 
+// along by separating with a comma same way you define multiple variables. The 
+// order must be in the same way as the parameter */
+
+// /* We can assign a default value by declaring the function with a name value pair method.
+// This means that whenever you call the function with no value (), it will automatically
+// fall back to use the default values.*/
+
+// /* Return gives a value that is stored and can be retrieved later as a const. Return unlike 
+// console.log does not need parenthesis. You return directly */
+
+// const greet = function(){
+//     return 'hello world';
+// }
+
+
+
+
+// const greet = hello => 'hello world';
+// const hello = greet(hello);
+
+
